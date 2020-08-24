d1768d7332ddf767c6a35a7b4390617c78624b8b
Andrii <Luckyhidalgo@gmail.com>
2020-08-24 13:40:18 +0300

    Career page: - created main section; - fixed media section;

--- dest/js/app.js
+++ dest/js/app.js
@@ -99,6 +99,31 @@ var initPopups = function initPopups() {
 			close: function close() {}
 		}
 	});
+
+	$('[popup-gallery-js]').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		fixedContentPos: true,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'is-show',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+		callbacks: {
+			beforeOpen: function beforeOpen() {
+				this.st.mainClass = this.st.el.attr('data-effect');
+			},
+			close: function close() {}
+		}
+	});
 };
 
 /**
--- src/js/_lib/magnificPopup.js
+++ src/js/_lib/magnificPopup.js
@@ -43,4 +43,29 @@ const initPopups = () => {
     }
   });
 
+	$('[popup-gallery-js]').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		fixedContentPos: true,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'is-show',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		callbacks: {
+			beforeOpen: function() {
+				this.st.mainClass = this.st.el.attr('data-effect');
+			},
+			close: function() {}
+		}
+	});
+
 };
